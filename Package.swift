75cef91aa6522a6e310a8decda0fbbefe6244f27
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-12-13 00:16:32 -0600

    [UPDATE] - Se agrega soporte a iOS 14 y el path para hacer visibles las clases de la carpeta TestController

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "DependenciesPackage",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -36,7 +39,8 @@ let package = Package(
                 .product(name: "FirebasePerformance", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
-            ]
+            ],
+            path: "Sources/TestController"
         ),
         .testTarget(
             name: "DependenciesPackageTests",
